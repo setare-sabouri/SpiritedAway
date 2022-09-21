08c4513371431b491a04a14e2818d478148fea67
setare <setarehsaboory@gmail.com>
2022-09-21 11:22:42 +0200

    Game page adeed

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
-const audio=document.getElementById("pageAudio");
-console.log("bejnkbkjbkj");
+const gameItem=document.querySelectorAll(".game-item");
+const gameBtn=document.getElementById("game-btn");
+const gameContainer=document.getElementById("game-container");
+
+//----------------------game.html Js-------------------------
+
+for (let i = 0; i < gameItem.length; i++) {
+    gameItem[i].addEventListener('click',function () {
+        alert("YOu are selecting"+" item "+ (i+1) );
+        const img = gameItem[i].querySelector('img');
+        const src = img.getAttribute('src');
+        const alt = img.getAttribute('alt');
+        console.log(alt);
+        console.log(src);
+     }); 
+}
+
+//--------------------GamePage.html Js--------------------
+function createCharacter() {
+    const NeweChar=document.createElement('div');
+    NeweChar.innerHTML="i am noface";
+    gameContainer.appendChild(NeweChar);
+    console.log('here you areeee');
+}
+function startGame(){
+    createCharacter();
+}
+function playgame() {
+    startGame();
+
+}
+
+
+
+const SelectBtn=document.getElementById("Select-btn");
+SelectBtn.addEventListener('click',function (params) {
+    SelectBtn.style.backgroundColor='Green';
+});
+ playgame();
