faeffc09c774254fdd66ed9d2dde01431a8be05f
setare <setarehsaboory@gmail.com>
2022-09-26 17:45:21 +0200

    Location and initial character set for game

--- js/game.js
+++ js/game.js
@@ -6,32 +6,18 @@ for (let i = 0; i < gameItem.length; i++) {
         const imgTaken = gameItem[i].querySelector('img');
         MyChar.src=imgTaken.getAttribute('src');
         MyChar.alt=imgTaken.getAttribute('alt');
+        console.dir(MyChar);
+        const myCharstring = JSON.stringify(MyChar);
+        localStorage.setItem('MyCharPassed', myCharstring);
     }); 
 }
 
-function gameBtn() {
-    const gameBtn = document.getElementById("gameBtn");
-    gameBtn.addEventListener('click', function () {
-    window.location.assign("../pages/GamePage.html");
-    })
-}
+// function gameBtn() {
+//     const gameBtn = document.getElementById("gameBtn");
+//     gameBtn.addEventListener('click', function () {
+//     window.location.assign("../pages/GamePage.html");
+//     })
+// }
 
 
-    const gameContainer=document.getElementById("game-container");
-    const gameContent=document.querySelector('.game-content');
-    
-    function createCharacter(MyChar) {
-        var NewChar=document.createElement('img');
-        NewChar.innerHTML="i was added";
-        console.log(MyChar.src);
-    }
-    function startGame(){
-        createCharacter();
-    }
-    
-        function playgame() {
-            console.log('hhhhh');
-            gameContent.innerHTML='dddddddddddddddddddddddd';
-        }
-    
     
--- js/main-game.js
+++ js/main-game.js
@@ -1,1 +1,32 @@
-console.log(MyChar);
+
+const gameContainer=document.getElementById("game-container");
+const gameContent=document.getElementById("game-content");
+
+const myCharTaken = localStorage.getItem('MyCharPassed');
+const GameChar = JSON.parse(myCharTaken);
+
+function initialChar() {
+    var NewChar=document.createElement('img');
+    NewChar.src=GameChar.src;
+    NewChar.alt=GameChar.alt;
+    NewChar.classList.add('game-character');
+    rndmRotation=Math.random() * 360;
+    NewChar.style.transform='rotate('+rndmRotation+'deg)';
+    NewChar=LocationGenerator(NewChar)
+    gameContent.appendChild(NewChar);
+    console.log(NewChar+" added");
+    console.log(gameContent.clientWidth);
+}
+function LocationGenerator(input){
+    var widthOfScreen=gameContent.clientWidth;
+    var heightOfScreen=gameContent.clientHeight;
+    input.style.top = Math.random() * widthOfScreen + 'px';
+    input.style.left=Math.random() * heightOfScreen +'px';
+    return input;
+    // const y = Math.random() * height;
+}
+function startGame(){
+    setTimeout(initialChar, 1000)
+}
+
+startGame();
